f1789e8733dfed6fdd005cf24f7cc9b74d5f1ce0
Mamoboldo <marcoboldetti@gmail.com>
2015-10-15 07:39:51 +0200

    Initial Commit

---
+++ To Painter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  To Painter
+//
+//  Created by Marco Boldetti on 15/10/15.
+//  Copyright © 2015 Marco Boldetti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
