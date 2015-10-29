85277e68e623a05f20b548e609b248a64f0e9cee
Mamoboldo <marcoboldetti@gmail.com>
2015-10-29 09:39:09 +0100

    Working...

---
+++ To Painter/QuotationsModel.swift
@@ -0,0 +1,19 @@
+//
+//  QuotationsModel.swift
+//  To Painter
+//
+//  Created by Marco Boldetti on 29/10/15.
+//  Copyright © 2015 Marco Boldetti. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class QuotationsModel: NSObject {
+    
+    var paintersName    : String! // Nome Imbianchino
+    var customerName    : String! // Nome Cliente
+    var date            : NSDate! // Data del preventivo
+    
+    var quotationTitle  : String! // Descrizione del preventivo
+}
